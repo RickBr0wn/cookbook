219375d16def3b7d726f6f2fc9ce96bb03091871
Rick Brown <ricky.brown.00@gmail.com>
2022-08-08 12:17:54 +0100

    Initial Commit

---
+++ cookbook/cookbookApp.swift
@@ -0,0 +1,17 @@
+//
+//  cookbookApp.swift
+//  cookbook
+//
+//  Created by Rick Brown on 08/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct cookbookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
